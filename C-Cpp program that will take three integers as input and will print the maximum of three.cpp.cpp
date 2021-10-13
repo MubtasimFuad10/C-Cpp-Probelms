7d4b0802a459718a6d91c70ab345768370fc225f
Mubtasim Fuad <53475627+MFT10@users.noreply.github.com>
2021-10-13 19:57:15 +0600

    Branching Related Problems in C/Cpp

---
+++ C-Cpp program that will take three integers as input and will print the maximum of three.cpp.cpp
@@ -0,0 +1,35 @@
+/**
+     *******  *     *     * *    * * *
+     *MFT10   *     *    *   *   *     *
+     *******  *     *   * * * *  *     *
+     *        *     *   *     *  *     *
+     *        * * * *   *     *  * * *
+**/
+
+#include<bits/stdc++.h>
+#define     lol             long long int //(positive,zero and negative)
+#define     ulol            unsigned long long int //(positive and zero)
+#define     sonic           ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     sf              scanf
+#define     pf              printf
+#define     PI              acos(-1)
+#define     rep(i,k,n)      for(long long int i=k;i<n;i++)
+#define     rep2(i,l,n)      for(long long int i=l;i<=n;i++)
+#define     max(a,b)        ((a) > (b) ? (a) : (b))
+#define     min(a,b)        ((a) < (b) ? (a) : (b))
+#define     end             "\n"
+using namespace std;
+int main()
+{
+    int a, b, c;
+    int max;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a>b) max=a;
+    else max=b;
+
+    if(max<c) max=c;
+
+    printf("maximum is: %d", max);
+
+
+}
