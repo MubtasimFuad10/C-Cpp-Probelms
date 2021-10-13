587a227feee4f09a8416ce171d630c9d11d41014
Mubtasim Fuad <53475627+MFT10@users.noreply.github.com>
2021-10-13 20:36:53 +0600

    Boolean in C/Cpp

---
+++ Working Step of Boolean in C-Cpp.cpp
@@ -0,0 +1,36 @@
+/**
+     *******  *     *     * *    * * *
+     *MFT10   *     *    *   *   *     *
+     *******  *     *   * * * *  *     *
+     *        *     *   *     *  *     *
+     *        * * * *   *     *  * * *
+**/
+
+
+#define     lol             long long int //(positive,zero and negative)
+#define     ulol            unsigned long long int //(positive and zero)
+#define     sonic           ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     sf              scanf
+#define     pf              printf
+#define     PI              acos(-1)
+#define     rep(i,k,n)      for(long long int i=k;i<n;i++)
+#define     rep2(i,l,n)      for(long long int i=l;i<=n;i++)
+#define     max(a,b)        ((a) > (b) ? (a) : (b))
+#define     min(a,b)        ((a) < (b) ? (a) : (b))
+#define     end             "\n"
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+                                 ///*** Boolean in C/Cpp ***\\\
+
+                /// Zero (0)           --->         False
+                ///Anything nonzero    --->         TRUE
+
+int main()
+{
+    int a=5;
+    printf("%d\n", a>5); //Output: 0 (which means False in C compiler)
+      printf("%d", a==5); //Output: 1 (which means True in C compiler)
+}
