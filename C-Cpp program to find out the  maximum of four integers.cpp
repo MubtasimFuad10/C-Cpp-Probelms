98306689166dcf0e9e66488e1b511f13ddaab26b
Mubtasim Fuad <53475627+MFT10@users.noreply.github.com>
2021-10-13 20:03:58 +0600

    Branching Related Problems in C/Cpp

---
+++ C-Cpp program to find out the  maximum of four integers.cpp
@@ -0,0 +1,39 @@
+/**
+     *******  *     *     * *    * * *
+     *MFT10   *     *    *   *   *     *
+     *******  *     *   * * * *  *     *
+     *        *     *   *     *  *     *
+     *        * * * *   *     *  * * *
+**/
+
+
+#define     lol             long long int //(positive,zero and negative)
+#define     ulol            unsigned long long int //(positive and zero)
+#define     sonic           ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     sf              scanf
+#define     pf              printf
+#define     PI              acos(-1)
+#define     rep(i,k,n)      for(long long int i=k;i<n;i++)
+#define     rep2(i,l,n)      for(long long int i=l;i<=n;i++)
+#define     max(a,b)        ((a) > (b) ? (a) : (b))
+#define     min(a,b)        ((a) < (b) ? (a) : (b))
+#define     end             "\n"
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int max;
+    if(a>b) max=a;
+    else max=b;
+
+    if(max<c) max=c;
+     if(max<d) max=d;
+
+    printf("Maximum is: %d", max);
+
+}
