e65ae63cdcc27cb38607a0fdab2e59eabce1a48c
Mubtasim Fuad <53475627+MFT10@users.noreply.github.com>
2021-10-13 20:11:54 +0600

    Branching Related Problems in C/Cpp

---
+++ C-Cpp prgram to find if year is leap year or not leap year.cpp
@@ -0,0 +1,36 @@
+/**
+     *******  *     *     * *    * * *
+     *MFT10   *     *    *   *   *     *
+     *******  *     *   * * * *  *     *
+     *        *     *   *     *  *     *
+     *        * * * *   *     *  * * *
+**/
+
+
+#define     lol             long long int //(positive,zero and negative)
+#define     ulol            unsigned long long int //(positive and zero)
+#define     sonic           ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     sf              scanf
+#define     pf              printf
+#define     PI              acos(-1)
+#define     rep(i,k,n)      for(long long int i=k;i<n;i++)
+#define     rep2(i,l,n)      for(long long int i=l;i<=n;i++)
+#define     max(a,b)        ((a) > (b) ? (a) : (b))
+#define     min(a,b)        ((a) < (b) ? (a) : (b))
+#define     end             "\n"
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int y;
+    scanf("%d", &y);
+    if(y%400==0 || y%4==0 && y%100 !=0)
+        printf("Leap year\n");
+        else
+            printf("Not a Leap Year\n");
+        return 0;
+
+}
