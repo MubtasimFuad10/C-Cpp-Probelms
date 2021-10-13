029cd766dd11ddbc79d66a448c019bc33972ace0
Mubtasim Fuad <53475627+MFT10@users.noreply.github.com>
2021-10-13 20:35:04 +0600

    Valid Triangle in C/Cpp

---
+++ C-Cpp Program to find out valid triangle.cpp
@@ -0,0 +1,34 @@
+/**
+     *******  *     *     * *    * * *
+     *MFT10   *     *    *   *   *     *
+     *******  *     *   * * * *  *     *
+     *        *     *   *     *  *     *
+     *        * * * *   *     *  * * *
+**/
+
+
+#define     lol             long long int //(positive,zero and negative)
+#define     ulol            unsigned long long int //(positive and zero)
+#define     sonic           ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     sf              scanf
+#define     pf              printf
+#define     PI              acos(-1)
+#define     rep(i,k,n)      for(long long int i=k;i<n;i++)
+#define     rep2(i,l,n)      for(long long int i=l;i<=n;i++)
+#define     max(a,b)        ((a) > (b) ? (a) : (b))
+#define     min(a,b)        ((a) < (b) ? (a) : (b))
+#define     end             "\n"
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a+b>c && b+c>a && c+a>b)
+        cout << "Valid Triangle" << endl;
+    else
+        cout << "InValid Triangle" << endl;
+}
