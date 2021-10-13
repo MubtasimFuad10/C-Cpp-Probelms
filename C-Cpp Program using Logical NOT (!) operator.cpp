5dd2562ec731f90769f55b6ba4c54826a849ddb0
Mubtasim Fuad <53475627+MFT10@users.noreply.github.com>
2021-10-13 20:15:45 +0600

    Branching Related Problems in C/Cpp

---
+++ C-Cpp Program using Logical NOT (!) operator.cpp
@@ -0,0 +1,39 @@
+/**
+     *******  *     *     * *    * * *
+     *MFT10   *     *    *   *   *     *
+     *******  *     *   * * * *  *     *
+     *        *     *   *     *  *     *
+     *        * * * *   *     *  * * *
+**/
+
+
+#define     lol             long long int //(positive,zero and negative)
+#define     ulol            unsigned long long int //(positive and zero)
+#define     sonic           ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     sf              scanf
+#define     pf              printf
+#define     PI              acos(-1)
+#define     rep(i,k,n)      for(long long int i=k;i<n;i++)
+#define     rep2(i,l,n)      for(long long int i=l;i<=n;i++)
+#define     max(a,b)        ((a) > (b) ? (a) : (b))
+#define     min(a,b)        ((a) < (b) ? (a) : (b))
+#define     end             "\n"
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+///*** Using Logical NOT (!) operator
+int main()
+{
+    float marks;
+    cin >> marks;
+    if(!(marks<60)) //important
+    {
+        printf("Passed\n");
+    }
+    else
+    {
+        printf("Failed");
+    }
+}
