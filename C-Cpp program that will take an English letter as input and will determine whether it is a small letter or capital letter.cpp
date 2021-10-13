73774872309639efc3d4c54df76c0bc7ccee1953
Mubtasim Fuad <53475627+MFT10@users.noreply.github.com>
2021-10-13 20:32:34 +0600

    Char Related Problems in C/Cpp

---
+++ C-Cpp program that will take an English letter as input and will determine whether it is a small letter or capital letter.cpp
@@ -0,0 +1,45 @@
+/**
+     *******  *     *     * *    * * *
+     *MFT10   *     *    *   *   *     *
+     *******  *     *   * * * *  *     *
+     *        *     *   *     *  *     *
+     *        * * * *   *     *  * * *
+**/
+
+
+#define     lol             long long int //(positive,zero and negative)
+#define     ulol            unsigned long long int //(positive and zero)
+#define     sonic           ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define     sf              scanf
+#define     pf              printf
+#define     PI              acos(-1)
+#define     rep(i,k,n)      for(long long int i=k;i<n;i++)
+#define     rep2(i,l,n)      for(long long int i=l;i<=n;i++)
+#define     max(a,b)        ((a) > (b) ? (a) : (b))
+#define     min(a,b)        ((a) < (b) ? (a) : (b))
+#define     end             "\n"
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+     char t;
+     scanf("%c", &t);
+     //if(t>=65 && t<=90)
+        if(t>='A' && t<='Z')
+     {
+         printf("Capital Letter.\n");
+
+     }
+    // else if(t>=97 && t<=122)
+        else if(t>='a' && t<='z')
+     {
+         printf("Small Letter.\n");
+     }
+     else
+     {
+         printf("Not a Letter.\n");
+     }
+ }
