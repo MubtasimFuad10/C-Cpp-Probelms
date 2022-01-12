5b6cb5c5d406b31a9c6f64246f503bf906679dc7
Mubtasim Fuad <53475627+MFT10@users.noreply.github.com>
2022-01-13 02:07:09 +0600

    Strings

--- String.cpp
+++ String.cpp
@@ -85,6 +85,162 @@ int main(){
     cout << "your name is: " << fullName;
 }
 
+//using scanf(but space will not work either in scanf)
+                        int main()
+                                {
+                                    char t[27];
+                                    puts("please enter a string: ");
+                                    scanf("%15s", t); 
+                                    puts("You have entered: ");
+                                    puts(t);
 
+
+                                }
+///using scanf to get a string
+                    int main()
+                        {
+                            char t[27];
+                            puts("please enter a string: ");
+                           scanf("%[^abcd]",t); //this means abcd badd diye shob print koro.
+                            int a=strlen(t);
+                            puts("You have entered: ");
+                            puts(t);
+                            printf("%d", a);
+
+
+                        }
+
+                    int main()
+                        {
+                            char t[27];
+                            puts("please enter a string: ");
+
+                           scanf("%[^\n]", t);
+                            puts("You have entered: ");
+                            puts(t);
+
+
+                        }
+///using scanf() declaring number of output to print
+                    int main()
+                        {
+                            char t[27];
+                            puts("please enter a string: ");
+
+                           scanf("%26[^\n]", t);//declaring space using scanf
+                            puts("You have entered: ");
+                            puts(t);
+
+
+                        }
+
+//Write down a program that will print n-th letter in a sentence entered by a user. n will be input to your program
+                     int main()
+                            {
+                                char a[10];
+                                int i,l,n;
+
+                                gets(a);
+                                l=strlen(a);
+                                scanf("%d", &n);
+                                    if(n>l-1) printf("no character");
+
+                                    printf("%c", a[n]);
+
+                            }
+
+
+///vertically letter print code
+
+                int main()
+                        {
+                            char a[10];
+                            gets(a);
+                            int i;
+                            for(i=0;i<10;i++)
+                            {
+                                printf("%c\n", a[i]);
+                            }
+                        }
+
+///searching a letter in a string
+                int main()
+                    {
+                        char s[80], t;
+                        int i, n, l, p;
+                        gets(s);
+                        printf("Which letter?");
+                        scanf("%c" , &t);
+                        l=strlen(s);
+                        p=-1;
+                        for(i=0;i<l;i++)
+                        {
+                            if(s[i]==t) p=i;
+
+                        }
+                        if(p==-1) printf("Not Found");
+                        else printf("Found at %d", p);
+
+                    }
+///All library Function of string
+
+                int main()
+                    {
+                        char a[10];
+                        gets(a);
+                        strrev(a);//reverse the word
+                        strupr(a);//uppercase the string
+                        strlwr(a);//lowercase the string
+                        printf("%s\n", a);
+
+
+                        char s1[30]="Bad";
+                        char s2[30]="Good";
+                        strcpy(s1,s2); //copy the 2nd string to first. like s1=s2;
+                        printf("%s\n", s2);
+
+
+                        char s3[30]="Coastal";
+                        char s4[30]="cry";
+                        strncpy(s3,s4,3);//copy only 3 letter of s4 to s3.
+                        printf("%s\n", s3);
+
+
+                        char s5[30]="Hello ";
+                        char s6[30]="world";
+                        strcat(s5,s6); //append s6 and s5 . output : hello world
+                        printf("%s\n", s5);
+
+
+                        char s7[30]="yo";
+                        char s8[30]="Happy";
+                        strncat(s7,s8,6); //append 6 letter of s8 to s7.
+                        printf("%s\n", s7);
+
+
+                        int x=strcmp(s7,s8);
+                        int x=strcmpi(s7,s8);//strcmpi() ignores the alphabetic order of a string
+                        if(x==0) printf("same");
+                        else if(x>0) printf("Positive.s1>s2");
+                        else if(x<0) printf("Negetive.s1<s2");
+
+
+                        return 0;
+                    }
+
+
+///palindrome string
+                int main()
+                        {
+                            char s[80];
+                            gets(s);
+                            char t[80];
+                            strcpy(t,s);
+                            strrev(s);
+                            if(strcmpi(s,t)==0)
+                                printf("%s is a palindrom", s);
+                            else
+                                printf("%s is not a palindrom", s);
+                        }
 
 
